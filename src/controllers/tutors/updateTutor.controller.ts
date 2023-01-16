31084d007c91cacd6c19f746e311529b6f04d256
Gabriel Correa Costa <103125844+Biellokos2@users.noreply.github.com>
2023-01-16 11:00:18 -0300

    feat: tutors/Create and Update

--- src/controllers/tutors/updateTutor.controller.ts
+++ src/controllers/tutors/updateTutor.controller.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from "express";
-import { tutorUpdateService } from "../../services/patients/updateTutor.service";
+import { tutorUpdateService } from "../../services/tutors/updateTutor.service";
 
 const tutorUpdateController = async (req:Request, res:Response) => {
     const tutorId = req.params.id
     const tutorData = req.body
-    const tutorOwnerId = req.body.user.id
-    const updatedTutor = await tutorUpdateService(tutorData,tutorId,tutorOwnerId)
+    const updatedTutor = await tutorUpdateService(tutorData,tutorId)
     return res.status(200).json(updatedTutor)
 }
 export {tutorUpdateController}
--- src/routes/patientsRouter.ts
+++ src/routes/patientsRouter.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { patientsCreateController } from "../controllers/patients/createPatient.controller";
 import { patientsSessionController } from "../controllers/patients/sessionPatient.controller";
-import { TutorCreateController } from "../controllers/patients/createTutor.controller";
 import { createAddressPatientController } from "../controllers/patients/createAddressPatient.controller";
 import { patientDataRequestSchema, patientSessionRequestSchema, tutorDataRequestSchema} from "../schemas/patients";
 import { verifyBodyRequestMiddleware } from "../middlewares/Global/verifyBodyRequest.middleware";
 import { patientsUpdateController } from "../controllers/patients/updatePatients.controller";
 import { ensureAuthMiddleware } from "../middlewares/Global/ensureAuth.middleware";
 import { patientsDeleteController } from "../controllers/patients/deletePatient.controller";
-import { tutorUpdateController } from "../controllers/patients/updateTutor.controller";
+import { tutorUpdateController } from "../controllers/tutors/updateTutor.controller";
+import { TutorCreateController } from "../controllers/tutors/createTutor.controller";
 
 
 const routePatients = Router()
